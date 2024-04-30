a4985061ea3a36db403a18be70e5bfa41ad37da6
ezazee <yukiirima123@gmail.com>
2024-05-01 00:01:18 +0700

    asdasd

--- components/HotjarScript.tsx
+++ components/HotjarScript.tsx
@@ -5,18 +5,15 @@ const HotjarScript = () => {
   useEffect(() => {
     // Add Hotjar tracking code here
     const hotjarScript = document.createElement("script");
-    hotjarScript.innerHTML = `
-      <!-- Hotjar Tracking Code for Zaportfolio -->
-    <script>
-    (function(h,o,t,j,a,r){
+    hotjarScript.textContent = `
+      (function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
         h._hjSettings={hjid:4964709,hjsv:6};
         a=o.getElementsByTagName('head')[0];
         r=o.createElement('script');r.async=1;
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
         a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-    </script>
+      })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
     `;
     document.head.appendChild(hotjarScript);
 
